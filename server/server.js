19dbd5dc5217b926707c2960cfc43b034c75a934
chandredewet <chandredewet@gmail.com>
2022-11-19 13:53:08 +0200

    Initial push

---
+++ server/server.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 3001;
+
+//require the JSON files in the data folder
+const Harrow = require("./data/Harrow.json");
+const Heathrow = require("./data/Heathrow.json");
+const Stratford = require("./data/Stratford.json");
+
+//routes
+app.get("/", (request, response) => {
+  response.send({
+    SupportedRoutes: ["/Harrow", "/Heathrow", "/Stratford"],
+    Version: "0.0.0.1",
+    Author: "cdewet",
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
